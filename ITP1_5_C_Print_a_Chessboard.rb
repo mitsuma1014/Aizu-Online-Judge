c985dbce7be41b354bc96e1cee586e8568b932a7
mitsuma1014 <65802385+mitsuma1014@users.noreply.github.com>
2020-09-07 20:19:33 +0900

    回答

--- ITP1_5_C_Print_a_Chessboard.rb
+++ ITP1_5_C_Print_a_Chessboard.rb
@@ -1,2 +1,32 @@
-hello
-test
+h,w = gets.split(" ").map(&:to_i)
+while !(h == 0 && w == 0) do
+  if w.even?
+    if h.even? 
+      (h/2).times do
+        puts "#." * (w/2)
+        puts ".#" * (w/2)
+      end
+    else
+      puts "#." * (w/2)
+      (h/2).times do
+        puts ".#" * (w/2)
+        puts "#." * (w/2)
+      end
+    end
+  else #よこが奇数の場合
+    if h.even? #縦が偶数の場合
+      (h/2).times do
+        puts "#" + ".#" * (w/2)
+        puts "." + "#." * (w/2)
+      end
+    else #縦が奇数の場合
+      puts "#" + ".#" * (w/2)
+      (h/2).times do
+        puts "." + "#." * (w/2)
+        puts "#" + ".#" * (w/2)
+      end
+    end
+  end  
+  puts ""
+  h,w = gets.split(" ").map(&:to_i)
+end
